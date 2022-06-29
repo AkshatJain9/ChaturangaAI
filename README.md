b706d8af12e2eaa7c56c9430c6b3b75c49720a12
Akshat Jain <u7284072@anu.edu.au>
2022-06-29 10:33:46 +1000

    Added Board Image

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 A C++ Implementation of an Chaturanga (Ancient Indian Chess Ancestor) AI
 
 ### Run Command (Root)
-g++ src/*.cpp -o main.exe -I lib/SDL2_lib/include -L lib/sdl2_lib/libr -L lib/sdl2_lib -lsdl2
+g++ src/*.cpp -o main.exe -I lib/SDL2_lib/include -Llib/sdl2_lib/libr -Llib/sdl2_lib -lsdl2 -lsdl2_image
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,50 @@
 #include <iostream>
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
+#include <SDL_image.h>
 
 int WIDTH = 800;
 int HEIGHT = 800;
 
 int main(int argc, char *argv[])
 {   
-    std::cout << "Hello Worlasdasd" << std::endl;
+    
+    if (SDL_Init(SDL_INIT_VIDEO) > 0) {
+        std::cout << "SDL INIT FAILED" << SDL_GetError() << std::endl;
+    }
 
-    SDL_Window *window = SDL_CreateWindow( "Hello SDL WORLD", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI );
+    SDL_Window* window = SDL_CreateWindow("Chaturanga", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    if ( NULL == window )
+    if (NULL == window)
     {
-        std::cout << "Could not create window: " << SDL_GetError( ) << std::endl;
+        std::cout << "Could not create window: " << SDL_GetError() << std::endl;
         return 1;
+    }
+
+    SDL_Texture* boardTexture = IMG_LoadTexture(renderer, "res/images/board.png");
+
+    if (boardTexture == NULL) {
+        std::cout << "Failed to load texture. Error: " << SDL_GetError() << std::endl;
     }
 
     SDL_Event windowEvent;
 
-    while ( true )
+    while (true)
     {
-        if ( SDL_PollEvent( &windowEvent ) )
+        if (SDL_PollEvent(&windowEvent))
         {
-            if ( SDL_QUIT == windowEvent.type )
-            { break; }
+            if (SDL_QUIT == windowEvent.type)
+            {break;}
         }
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, boardTexture, NULL, NULL);
+        SDL_RenderPresent(renderer);
     }
 
-    SDL_DestroyWindow( window );
-    SDL_Quit( );
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return EXIT_SUCCESS;
 }
